0722e70e07ac3cf505b5b2837e5c8131007a5031
ChefData <150172835+ChefData@users.noreply.github.com>
2023-12-08 19:29:48 +0000

    Update database_utils.py
    Simplifies file by removing duplicate code

--- database_utils.py
+++ database_utils.py
@@ -14,7 +14,7 @@ class DatabaseConnector:
     - engine: SQLAlchemy engine for the main database connection.
     """
 
-    def __init__(self, db_creds_file='do_not_track/db_creds.yaml', pg_creds_file='do_not_track/pg_creds.yaml'):
+    def __init__(self, db_creds_file):
         """
         Initialize the DatabaseConnector.
 
@@ -23,9 +23,7 @@ class DatabaseConnector:
         - local_creds_file (str): Path to the YAML file containing local database credentials.
         """
         self.db_creds_file = db_creds_file
-        self.pg_creds_file = pg_creds_file
         self.db_engine = self.init_db_engine()
-        self.pg_engine = self.init_pg_engine()
         self.logger = self.setup_logging()
 
     def setup_logging(self):
@@ -34,85 +32,46 @@ class DatabaseConnector:
 
     def read_db_creds(self):
         """
-        Read AWS database credentials from the YAML file.
+        Read database credentials from the YAML file.
 
         Parameters:
-        - db_creds_file (str): Path to the YAML file containing AWS database credentials.
+        - db_creds_file (str): Path to the YAML file containing database credentials.
 
         Returns:
-        - dict: AWS database credentials.
+        - dict: Database credentials.
         """
         try:
             with open(self.db_creds_file, 'r') as file:
                 db_creds = yaml.safe_load(file)
             return db_creds
         except FileNotFoundError as e:
-            self.logger.error(f"Error: AWS database credentials file '{self.db_creds_file}' not found.")
-            raise FileNotFoundError(f"Error: AWS database credentials file '{self.db_creds_file}' not found.") from e
+            self.logger.error(f"Error: database credentials file '{self.db_creds_file}' not found.")
+            raise FileNotFoundError(f"Error: database credentials file '{self.db_creds_file}' not found.") from e
         except yaml.YAMLError as e:
             self.logger.error(f"Error: Unable to load YAML from '{self.db_creds_file}': {e}")
             raise ValueError(f"Error: Unable to load YAML from '{self.db_creds_file}': {e}") from e
 
-    def read_pg_creds(self):
-        """
-        Read PostgreSQL database credentials from the YAML file.
-
-        Parameters:
-        - pg_creds_file (str): Path to the YAML file containing PostgreSQL database credentials.
-
-        Returns:
-        - dict: PostgreSQL database credentials.
-        """
-        try:
-            with open(self.pg_creds_file, 'r') as file:
-                pg_creds = yaml.safe_load(file)
-            return pg_creds
-        except FileNotFoundError as e:
-            self.logger.error(f"Error: PostgreSQL database credentials file '{self.pg_creds_file}' not found.")
-            raise FileNotFoundError(f"Error: PostgreSQL database credentials file '{self.pg_creds_file}' not found.") from e
-        except yaml.YAMLError as e:
-            self.logger.error(f"Error: Unable to load YAML from '{self.pg_creds_file}': {e}")
-            raise ValueError(f"Error: Unable to load YAML from '{self.pg_creds_file}': {e}") from e
-
     def create_db_url(self):
         """
-        Creates a AWS database URL from the provided credentials.
-
-        Returns:
-            str: AWS database URL for SQLAlchemy engine.
-        """
-        try:
-            db_creds = self.read_db_creds()
-            if any(key not in db_creds for key in ['DRIVER', 'USER', 'PASSWORD', 'HOST', 'PORT', 'DATABASE']):
-                self.logger.error("Error: AWS database credentials are missing or incomplete.")
-                raise ValueError("Error: AWS database credentials are missing or incomplete.")
-            db_url = f"{db_creds['DRIVER']}://{db_creds['USER']}:{db_creds['PASSWORD']}@{db_creds['HOST']}:{db_creds['PORT']}/{db_creds['DATABASE']}"
-            return db_url
-        except Exception as e:
-            self.logger.error(f"Error creating AWS database URL: {e}")
-            raise ValueError(f"Error creating AWS database URL: {e}")
-    
-    def create_pg_url(self):
-        """
-        Creates a PostgreSQL database URL from the provided credentials.
+        Creates a database URL from the provided credentials.
 
         Returns:
             str: Database URL for SQLAlchemy engine.
         """
         try:
-            pg_creds = self.read_pg_creds()
-            if any(key not in pg_creds for key in ['DRIVER', 'USER', 'PASSWORD', 'HOST', 'PORT', 'DATABASE']):
-                self.logger.error("Error: PostgreSQL database credentials are missing or incomplete.")
-                raise ValueError("Error: PostgreSQL database credentials are missing or incomplete.")
-            pg_url = f"{pg_creds['DRIVER']}://{pg_creds['USER']}:{pg_creds['PASSWORD']}@{pg_creds['HOST']}:{pg_creds['PORT']}/{pg_creds['DATABASE']}"
-            return pg_url
+            db_creds = self.read_db_creds()
+            if any(key not in db_creds for key in ['DRIVER', 'USER', 'PASSWORD', 'HOST', 'PORT', 'DATABASE']):
+                self.logger.error("Error: Database credentials are missing or incomplete.")
+                raise ValueError("Error: Database credentials are missing or incomplete.")
+            db_url = f"{db_creds['DRIVER']}://{db_creds['USER']}:{db_creds['PASSWORD']}@{db_creds['HOST']}:{db_creds['PORT']}/{db_creds['DATABASE']}"
+            return db_url
         except Exception as e:
-            self.logger.error(f"Error creating PostgreSQL database URL: {e}")
-            raise ValueError(f"Error creating PostgreSQL database URL: {e}")
-
+            self.logger.error(f"Error creating database URL: {e}")
+            raise ValueError(f"Error creating database URL: {e}")
+    
     def init_db_engine(self):
         """
-        Reads AWS database credentials, initializes, and returns a SQLAlchemy database engine.
+        Reads database credentials, initializes, and returns a SQLAlchemy database engine.
 
         Returns:
             sqlalchemy.engine.base.Engine: SQLAlchemy database engine.
@@ -125,26 +84,8 @@ class DatabaseConnector:
                 db_engine.execution_options(isolation_level='AUTOCOMMIT').connect()
                 return db_engine
         except SQLAlchemyError as e:
-            self.logger.error(f"Error initializing AWS database engine: {e}")
-            raise ValueError(f"Error initializing AWS database engine: {e}")
-
-    def init_pg_engine(self):
-        """
-        Reads PostgreSQL database credentials, initializes, and returns a SQLAlchemy database engine.
-
-        Returns:
-            sqlalchemy.engine.base.Engine: SQLAlchemy database engine.
-        """
-        try:
-            pg_url = self.create_pg_url()
-            if pg_url is not None:
-                pool_size = 5
-                pg_engine = create_engine(pg_url, pool_size=pool_size, poolclass=pool.QueuePool)
-                pg_engine.execution_options(isolation_level='AUTOCOMMIT').connect()
-                return pg_engine
-        except SQLAlchemyError as e:
-            self.logger.error(f"Error initializing PostgreSQL database engine: {e}")
-            raise ValueError(f"Error initializing PostgreSQL database engine: {e}")
+            self.logger.error(f"Error initializing database engine: {e}")
+            raise ValueError(f"Error initializing database engine: {e}")
 
     def list_db_tables(self):
         """
@@ -159,10 +100,10 @@ class DatabaseConnector:
                 table_names = inspector.get_table_names()
             return table_names
         except SQLAlchemyError as e:
-            self.logger.error(f"Error listing AWS database tables: {e}")
+            self.logger.error(f"Error listing database tables: {e}")
             return []
 
-    def upload_to_pg(self, df, table_name):
+    def upload_to_db(self, df, table_name):
         """
         Upload a DataFrame to a specified table in the local database.
 
@@ -171,9 +112,9 @@ class DatabaseConnector:
         - table_name (str): Name of the table in the database.
         """
         try:
-            df.to_sql(table_name, self.pg_engine, if_exists='replace', index=False)
+            df.to_sql(table_name, self.db_engine, if_exists='replace', index=False)
         except Exception as e:
-            print(f"Error uploading DataFrame to PostgreSQL database: {e}")
+            self.logger.error(f"Error uploading DataFrame to PostgreSQL database: {e}")
 
     def close_db_connection(self):
         """
@@ -182,27 +123,17 @@ class DatabaseConnector:
         try:
             if self.db_engine is not None:
                 self.db_engine.dispose()
-                self.logger.info("AWS database connection closed.")
+                self.logger.info("Database connection closed.")
         except SQLAlchemyError as e:
-            self.logger.error(f"Error closing AWS database connection: {e}")
-
-    def close_pg_connection(self):
-        """
-        Close the SQLAlchemy PostgreSQL database connection.
-        """
-        try:
-            if self.pg_engine is not None:
-                self.pg_engine.dispose()
-                self.logger.info("PostgreSQL database connection closed.")
-        except SQLAlchemyError as e:
-            self.logger.error(f"Error closing PostgreSQL database connection: {e}")
+            self.logger.error(f"Error closing database connection: {e}")
 
 if __name__ == "__main__":
     # Creating Class instances
     data_cleaner = DataCleaning()
     data_extractor = DataExtractor()
-    db_connector = DatabaseConnector()
-    
+    db_connector = DatabaseConnector('do_not_track/db_creds.yaml')
+    pg_connector = DatabaseConnector('do_not_track/pg_creds.yaml')
+
     # Reading and printing database credentials
     db_creds = db_connector.read_db_creds()
     print("Database Credentials:", db_creds)
@@ -220,15 +151,25 @@ if __name__ == "__main__":
     
     # Using read_rds_table to extract the table containing user data
     raw_data = data_extractor.read_rds_table(db_connector, user_data_table)
+    
+    # Printing the original DataFrame
+    print("User Data DataFrame:")
+    print(raw_data)
+    print(raw_data.info())
 
     # Cleaning user data
     cleaned_data = data_cleaner.clean_user_data(raw_data)
     
+    # Printing the cleaned DataFrame
+    print("Cleaned User Data:")
+    print(cleaned_data)
+    print(cleaned_data.info())
+
     # Uploading DataFrame to a specified table
     table_name_to_upload = 'dim_users'
-    db_connector.upload_to_pg(cleaned_data, table_name_to_upload)
+    pg_connector.upload_to_db(cleaned_data, table_name_to_upload)
     print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' database.")
     
     # Close the database connection
     db_connector.close_db_connection()
-    db_connector.close_pg_connection()
+    pg_connector.close_db_connection()
