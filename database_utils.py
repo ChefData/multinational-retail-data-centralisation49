48aaccbe1f5e96953e9e285d17589207a3c82def
ChefData <150172835+ChefData@users.noreply.github.com>
2023-12-10 13:15:55 +0000

    Update database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -1,18 +1,16 @@
 #%%
-import logging
 import yaml
 from sqlalchemy import create_engine, inspect, pool
 from sqlalchemy.exc import SQLAlchemyError
 from data_cleaning import DataCleaning
 from data_extraction import DataExtractor
 
-
 class DatabaseConnector:
     """
     A class for connecting to databases using SQLAlchemy.
 
     Attributes:
-    - engine: SQLAlchemy engine for the main database connection.
+        - db_engine: SQLAlchemy engine for the main database connection.
     """
 
     def __init__(self, db_creds_file):
@@ -20,37 +18,29 @@ class DatabaseConnector:
         Initialize the DatabaseConnector.
 
         Parameters:
-        - db_creds_file (str): Path to the YAML file containing database credentials.
-        - local_creds_file (str): Path to the YAML file containing local database credentials.
+            - db_creds_file (str): Path to the YAML file containing database credentials.
         """
         self.db_creds_file = db_creds_file
         self.db_engine = self.init_db_engine()
-        self.logger = self.setup_logging()
-
-    def setup_logging(self):
-        logging.basicConfig(level=logging.INFO)
-        return logging.getLogger(__name__)
 
     def read_db_creds(self):
         """
         Read database credentials from the YAML file.
 
-        Parameters:
-        - db_creds_file (str): Path to the YAML file containing database credentials.
-
         Returns:
-        - dict: Database credentials.
+            dict: Database credentials.
+        
+        Raises:
+            FileNotFoundError: If the YAML file is not found.
+            yaml.YAMLError: If there is an issue loading YAML from the file.
         """
         try:
             with open(self.db_creds_file, 'r') as file:
-                db_creds = yaml.safe_load(file)
-            return db_creds
+                return yaml.safe_load(file)
         except FileNotFoundError as e:
-            self.logger.error(f"Error: database credentials file '{self.db_creds_file}' not found.")
-            raise FileNotFoundError(f"Error: database credentials file '{self.db_creds_file}' not found.") from e
+            raise FileNotFoundError(f"Warning: database credentials file '{self.db_creds_file}' not found: {e}")
         except yaml.YAMLError as e:
-            self.logger.error(f"Error: Unable to load YAML from '{self.db_creds_file}': {e}")
-            raise ValueError(f"Error: Unable to load YAML from '{self.db_creds_file}': {e}") from e
+            raise yaml.YAMLError(f"Error: Unable to load YAML from '{self.db_creds_file}': {e}")
 
     def create_db_url(self):
         """
@@ -58,35 +48,35 @@ class DatabaseConnector:
 
         Returns:
             str: Database URL for SQLAlchemy engine.
+
+        Raises:
+            ValueError: If database credentials are missing or incomplete.
         """
         try:
             db_creds = self.read_db_creds()
-            if any(key not in db_creds for key in ['DRIVER', 'USER', 'PASSWORD', 'HOST', 'PORT', 'DATABASE']):
-                self.logger.error("Error: Database credentials are missing or incomplete.")
+            required_keys = ['DRIVER', 'USER', 'PASSWORD', 'HOST', 'PORT', 'DATABASE']
+            if any(key not in db_creds for key in required_keys):
                 raise ValueError("Error: Database credentials are missing or incomplete.")
-            db_url = f"{db_creds['DRIVER']}://{db_creds['USER']}:{db_creds['PASSWORD']}@{db_creds['HOST']}:{db_creds['PORT']}/{db_creds['DATABASE']}"
-            return db_url
-        except Exception as e:
-            self.logger.error(f"Error creating database URL: {e}")
+            return f"{db_creds['DRIVER']}://{db_creds['USER']}:{db_creds['PASSWORD']}@{db_creds['HOST']}:{db_creds['PORT']}/{db_creds['DATABASE']}"
+        except ValueError as e:
             raise ValueError(f"Error creating database URL: {e}")
-    
+
     def init_db_engine(self):
         """
         Reads database credentials, initializes, and returns a SQLAlchemy database engine.
 
         Returns:
             sqlalchemy.engine.base.Engine: SQLAlchemy database engine.
+
+        Raises:
+            SQLAlchemyError: If there is an error initializing the database engine.
         """
         try:
             db_url = self.create_db_url()
-            if db_url is not None:
-                pool_size = 5
-                db_engine = create_engine(db_url, pool_size=pool_size, poolclass=pool.QueuePool)
-                db_engine.execution_options(isolation_level='AUTOCOMMIT').connect()
-                return db_engine
+            engine_params = {'pool_size': 5, 'poolclass': pool.QueuePool}
+            return create_engine(db_url, **engine_params)
         except SQLAlchemyError as e:
-            self.logger.error(f"Error initializing database engine: {e}")
-            raise ValueError(f"Error initializing database engine: {e}")
+            raise SQLAlchemyError(f"Error initializing database engine: {e}")
 
     def list_db_tables(self):
         """
@@ -94,39 +84,34 @@ class DatabaseConnector:
 
         Returns:
             list: List of table names in the database.
+
+        Raises:
+            SQLAlchemyError: If there is an error listing database tables.
         """
         try:
-            with self.db_engine.execution_options(isolation_level='AUTOCOMMIT').connect() as connection:
+            with self.db_engine.connect() as connection:
                 inspector = inspect(connection)
-                table_names = inspector.get_table_names()
-            return table_names
+                return inspector.get_table_names()
         except SQLAlchemyError as e:
-            self.logger.error(f"Error listing database tables: {e}")
-            return []
+            raise SQLAlchemyError(f"Error listing database tables: {e}")
 
     def upload_to_db(self, df, table_name):
         """
         Upload a DataFrame to a specified table in the local database.
 
         Parameters:
-        - df (pd.DataFrame): DataFrame to upload.
-        - table_name (str): Name of the table in the database.
-        """
-        try:
-            df.to_sql(table_name, self.db_engine, if_exists='replace', index=False)
-        except Exception as e:
-            self.logger.error(f"Error uploading DataFrame to PostgreSQL database: {e}")
+            - df (pd.DataFrame): DataFrame to upload.
+            - table_name (str): Name of the table in the database.
 
-    def close_db_connection(self):
-        """
-        Close the SQLAlchemy database connection.
+        Raises:
+            SQLAlchemyError: If there is an error uploading the DataFrame to the database.
         """
         try:
-            if self.db_engine is not None:
-                self.db_engine.dispose()
-                self.logger.info("Database connection closed.")
+            with self.db_engine.connect() as connection:
+                df.to_sql(table_name, connection, if_exists='replace', index=False)
         except SQLAlchemyError as e:
-            self.logger.error(f"Error closing database connection: {e}")
+            raise SQLAlchemyError(f"Error uploading DataFrame to database: {e}")
+
 
 if __name__ == "__main__":
     # Creating Class instances
@@ -171,9 +156,3 @@ if __name__ == "__main__":
     pg_connector.upload_to_db(cleaned_data, table_name_to_upload)
     pg_engine = pg_connector.init_db_engine()
     print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' PostgreSQL database.\nPostgreSQL Database Engine: '{pg_engine}'")
-
-    # Close the database connection
-    db_connector.close_db_connection()
-    pg_connector.close_db_connection()
-
-# %%
