6e014880377abae0b8aa89c25241e7c8eb7e7e69
ChefData <150172835+ChefData@users.noreply.github.com>
2023-12-12 17:46:57 +0000

    Update export_data_user.py

--- export_data_user.py
+++ export_data_user.py
@@ -1,4 +1,3 @@
-#%%
 from data_cleaning import DataCleaning
 from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
@@ -47,4 +46,3 @@ if __name__ == "__main__":
     pg_connector.upload_to_db(cleaned_user_data, table_name_to_upload)
     pg_engine = pg_connector.init_db_engine()
     print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' PostgreSQL database.\nPostgreSQL Database Engine: '{pg_engine}'")
-# %%
