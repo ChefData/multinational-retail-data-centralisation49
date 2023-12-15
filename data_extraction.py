6fb004d22c7b4a01f723afc4fcd1074bce8dc6fc
Nick Armstrong <150172835+ChefData@users.noreply.github.com>
2023-12-15 19:56:40 +0000

    Update data_extraction.py
    Refactoring: access modifiers, consistent import order, consistent docstrings and comments, type annotations

--- data_extraction.py
+++ data_extraction.py
@@ -1,44 +1,77 @@
-import pandas as pd
-import tabula
-import requests
-import boto3
+# Import necessary modules for handling AWS services, PDF extraction, data manipulation, and HTTP requests
 from botocore.exceptions import NoCredentialsError, ClientError
+import boto3
 import json
+import pandas as pd
+import requests
+import tabula
 
 
 class DataExtractor:
     """
-    A class for extracting data from a database using a provided DatabaseConnector.
+    A class for extracting data from various sources such as databases, PDFs, APIs, and S3.
+
+    Attributes:
+    - header (dict): API key header.
+
+    Private Methods:
+    - __init__(self) -> None:
+        Initializes a DataExtractor object.
+    
+    Public Methods:
+    - read_rds_table(self, db_connector, table_name: str) -> pd.DataFrame:
+        Reads a table from a relational database and returns the data as a Pandas DataFrame.
+
+    - retrieve_pdf_data(self, pdf_link) -> pd.DataFrame:
+        Retrieves data from a PDF link and returns it as a Pandas DataFrame.
+
+    - set_api_key(self, api_key) -> None:
+        Sets the API key header.
+
+    - list_number_of_stores(self, number_stores_endpoint) -> int:
+        Retrieves the number of stores from an API endpoint.
+
+    - retrieve_stores_data(self, store_endpoint, number_of_stores) -> pd.DataFrame:
+        Retrieves store data from an API endpoint for a given number of stores and returns it as a Pandas DataFrame.
+
+    - extract_from_s3(self, s3_address) -> pd.DataFrame:
+        Extracts data from an S3 bucket based on the provided S3 address and returns it as a Pandas DataFrame.
     """
+
     def __init__(self):
+        """
+        Initializes a DataExtractor object.
+        """
+        # Constructor to initialize the API key header
         self.header = None
 
-    def read_rds_table(self, db_connector, table_name: str) -> pd.DataFrame:
+    @staticmethod
+    def read_rds_table(db_connector, table_name: str) -> pd.DataFrame:
         """
-        Read a table from a database using the provided DatabaseConnector.
+        Reads a table from a relational database and returns the data as a Pandas DataFrame.
 
         Parameters:
-            - db_connector: An instance of a DatabaseConnector.
-            - table_name: Name of the table to read.
+        - db_connector: An object with an '_init_db_engine' method for initializing a database engine.
+        - table_name (str): Name of the table in the database.
 
         Returns:
-            - Pandas DataFrame containing the data from the specified table.
-
+        - pd.DataFrame: Data from the specified table.
+        
         Raises:
-            - RuntimeError: If there is an error initializing the database engine.
-            - ValueError: If the specified table does not exist in the database.
-
+        - RuntimeError: If there is an error initializing the database engine.
+        - ValueError: If the specified table does not exist in the database.
         """
-        # Get the engine from the DatabaseConnector
+        # Get the database engine from the DatabaseConnector
         try:
-            db_engine = db_connector.init_db_engine()
-        except Exception as e:
-            raise RuntimeError("Error initializing database engine. Check your connection settings: {e}")
+            db_engine = db_connector._init_db_engine()
+        # Handle RuntimeError error
+        except RuntimeError as error:
+            raise RuntimeError(f"Error initializing database engine. Check your connection settings: {error}")
 
         # List tables in the database
         tables = db_connector.list_db_tables() 
         
-        # Check if the specified table exists
+        # Check if the specified table exists 
         if table_name not in tables: 
             raise ValueError(f"Table '{table_name}' not found in the database. Available tables: {tables}")
         
@@ -47,65 +80,108 @@ class DataExtractor:
             user_data_df = pd.read_sql_table(table_name, connection, index_col='index')
         return user_data_df
     
-    def retrieve_pdf_data(self, pdf_link):
+    @staticmethod
+    def retrieve_pdf_data(pdf_link) -> pd.DataFrame:
         """
-        Retrieve data from a PDF file.
+        Retrieves data from a PDF link and returns it as a Pandas DataFrame.
 
         Parameters:
-        - pdf_link (str): The file path or URL of the PDF.
+        - pdf_link (str): Link to the PDF file.
 
         Returns:
-        - pandas.DataFrame: A DataFrame containing the extracted data from the PDF.
+        - pd.DataFrame: Data from the PDF.
 
         Raises:
-        - FileNotFoundError: If the specified PDF file is not found.
-        - RuntimeError: If an unexpected error occurs during PDF processing.
+        - FileNotFoundError: If the PDF file is not found.
+        - RuntimeError: If there is an error processing the PDF.
         """
         try:
-            # Read pdf into list of DataFrame
+            # Read the PDF into a list of DataFrames
             pdf_data = tabula.read_pdf(pdf_link, pages='all', multiple_tables=True, lattice=True)
             
-            # Concatenate list of DataFrame into single dataframe
+            # Concatenate the list of DataFrames into a single DataFrame
             card_data_df = pd.concat(pdf_data)
             return card_data_df
         except FileNotFoundError:
             raise FileNotFoundError(f"The PDF file was not found @ link: '{pdf_link}'")
-        except Exception as e:
-            raise RuntimeError(f"An error occurred while processing the PDF: {e}")
+        except RuntimeError as error:
+            raise RuntimeError(f"An error occurred while processing the PDF: {error}")
 
-    def set_api_key(self, api_key):
+    def set_api_key(self, api_key) -> None:
+        """
+        Sets the API key header.
+
+        Parameters:
+        - api_key (str): API key.
+        """
+        # Method to set the API key for making authenticated requests to an API
         self.header = {'x-api-key': api_key}
 
     def list_number_of_stores(self, number_stores_endpoint):
+        """
+        Retrieves the number of stores from an API endpoint.
+
+        Parameters:
+        - number_stores_endpoint (str): API endpoint for retrieving the number of stores.
+
+        Returns:
+        - int: Number of stores.
+
+        Raises:
+        - requests.exceptions.RequestException: If there is an error retrieving the number of stores.
+        """
         try:
-            # Send a GET request to the API to retrieve information
-            response = requests.get(number_stores_endpoint, headers=self.header)
-            # Raise an HTTPError for bad responses (4xx or 5xx)
-            response.raise_for_status()
-            # Access the response data as JSON
-            number_of_stores = response.json().get('number_stores')
-            return number_of_stores
-        except requests.exceptions.RequestException as e:
-            raise requests.exceptions.RequestException(f"Error retrieving number of stores: {e} \nStatus code: {response.status_code} \nResponse Text: {response.text}")
-            
-    def retrieve_stores_data(self, store_endpoint, number_of_stores):
-        try:
-            all_stores_data = []
-            for store_number in range(1, number_of_stores):
-                # Send a GET request to the API endpoint for each store
-                response = requests.get(store_endpoint.format(store_number), headers=self.header)
+            with requests.Session() as session:
+                # Send a GET request to the API to retrieve information
+                response = session.get(number_stores_endpoint, headers=self.header)
                 # Raise an HTTPError for bad responses (4xx or 5xx)
-                response.raise_for_status()  
-                # Access the response data as JSON and Append to the list
-                store_data = response.json()
-                all_stores_data.append(store_data)
+                response.raise_for_status()
+                # Access the response data as JSON
+                number_of_stores = response.json().get('number_stores')
+                return number_of_stores
+        except requests.exceptions.RequestException as error:
+            raise requests.exceptions.RequestException(f"Error retrieving number of stores: {error} \nStatus code: {response.status_code} \nResponse Text: {response.text}")
+
+    def retrieve_stores_data(self, store_endpoint, number_of_stores) -> int:
+        """
+        Retrieves store data from an API endpoint for a given number of stores and returns it as a Pandas DataFrame.
+
+        Parameters:
+        - store_endpoint (str): API endpoint for retrieving store data.
+        - number_of_stores (int): Number of stores to retrieve.
+
+        Returns:
+        - pd.DataFrame: Store data.
+
+        Raises:
+        - requests.exceptions.RequestException: If there is an error retrieving store data.
+        """
+        try:
+            # Send a GET request to the API endpoint for each store and access the response data as JSON
+            all_stores_data = [requests.get(store_endpoint.format(store_number), headers=self.header).json()
+                               for store_number in range(0, number_of_stores)]
             # Create a DataFrame from the list of store data
             df = pd.DataFrame(all_stores_data)
             return df
-        except requests.exceptions.RequestException as e:
-            raise requests.exceptions.RequestException(f"Error retrieving store data: {e}")
-        
-    def extract_from_s3(self, s3_address):
+        except requests.exceptions.RequestException as error:
+            raise requests.exceptions.RequestException(f"Error retrieving store data: {error}")
+
+    @staticmethod
+    def extract_from_s3(s3_address) -> pd.DataFrame:
+        """
+        Extracts data from an S3 bucket based on the provided S3 address and returns it as a Pandas DataFrame.
+
+        Parameters:
+        - s3_address (str): S3 address specifying the bucket and key.
+
+        Returns:
+        - pd.DataFrame: Data from the S3 bucket.
+
+        Raises:
+        - NoCredentialsError: If AWS credentials are not found.
+        - ClientError: If there is an error related to AWS S3.
+        - ValueError: If the file format in the S3 address is not supported.
+        """
         try:
             # Create connection to S3 using default config and all buckets within S3. 's3' is a key word.
             s3 = boto3.client('s3')
@@ -116,14 +192,12 @@ class DataExtractor:
            # Get object and file (key) from bucket
             response = s3.get_object(Bucket= bucket, Key= key) 
 
-            # Create a DataFrame from S3 csv file. 'Body' is a key word
+            # Create a DataFrame from S3 file. 'Body' is a key word
             data = response['Body']
 
-            # Check if the data is in CSV format based on file extension
+            # Check the file format based on the file extension
             if s3_address.endswith('.csv'):
                 df = pd.read_csv(data)
-            
-            # Check if the data is in JSON format based on file extension
             elif s3_address.endswith('.json'):
                 # Read the content of the StreamingBody as bytes
                 data_read = data.read()
@@ -135,12 +209,13 @@ class DataExtractor:
             # Handle other formats or raise an error if needed
             else:
                 raise ValueError(f"Unsupported file format for S3 address: {s3_address}")
+            
             return df
         
-        except NoCredentialsError as e:
-            raise NoCredentialsError(f"AWS credentials not found. Please configure your credentials: {e}")
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'NoSuchBucket':
-                raise ClientError("The specified bucket does not exist: {e}")
+        except NoCredentialsError as error:
+            raise NoCredentialsError(f"AWS credentials not found. Please configure your credentials: {error}")
+        except ClientError as error:
+            if error.response['Error']['Code'] == 'NoSuchBucket':
+                raise ClientError(f"The specified bucket does not exist: {error}")
             else:
-                raise ClientError("An error occurred: {e}")
+                raise ClientError(f"An error occurred: {error}")
