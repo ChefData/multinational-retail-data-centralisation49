8e0f01c57e8ce4a3834b36110e0317b189729a2c
Nick Armstrong <150172835+ChefData@users.noreply.github.com>
2023-12-15 19:50:26 +0000

    Update export_data_rds_orders.py
    Adds Access Modifiers

--- export_data_rds_orders.py
+++ export_data_rds_orders.py
@@ -2,20 +2,13 @@ from data_cleaning import DataCleaning
 from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
 
+
 if __name__ == "__main__":
     # Creating Class instances
     data_cleaner = DataCleaning()
     data_extractor = DataExtractor()
     db_connector = DatabaseConnector('do_not_track/db_creds.yaml')
     pg_connector = DatabaseConnector('do_not_track/pg_creds.yaml')
-
-    # Reading and printing database credentials
-    db_creds = db_connector.read_db_creds()
-    print("Database Credentials:", db_creds)
-
-    # Initializing and printing the database engine
-    db_engine = db_connector.init_db_engine()
-    print("Database Engine:", db_engine)
 
     # Listing and printing all tables in the database
     db_tables = db_connector.list_db_tables()
@@ -43,5 +36,4 @@ if __name__ == "__main__":
     # Uploading DataFrame to a specified table
     table_name_to_upload = 'orders_table'
     pg_connector.upload_to_db(cleaned_orders_data, table_name_to_upload)
-    pg_engine = pg_connector.init_db_engine()
-    print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' PostgreSQL database.\nPostgreSQL Database Engine: '{pg_engine}'")
+    print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' PostgreSQL database.")
