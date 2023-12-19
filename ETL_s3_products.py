f17c8e2a85fea7c966d63b51a07d47ce318b7e3b
Nick Armstrong <150172835+ChefData@users.noreply.github.com>
2023-12-19 20:54:32 +0000

    Update ETL_s3_products.py
    Adds casting of data types and setting of primary keys

--- ETL_s3_products.py
+++ ETL_s3_products.py
@@ -31,3 +31,23 @@ if __name__ == "__main__":
     table_name_to_upload = 'dim_products'
     pg_connector.upload_to_db(cleaned_products_data, table_name_to_upload)
     print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' PostgreSQL database.")
+
+    # Cast data types - The ? in VARCHAR will be replaced with an integer representing the maximum length of the values in that column.
+    column_types = {
+        'product_price_£': 'FLOAT',
+        'weight_kg'      : 'FLOAT',
+        'int_article_no' : 'VARCHAR(?)',
+        'product_code'   : 'VARCHAR(?)',
+        'weight_class'   : 'VARCHAR(?)',
+        'date_added'     : 'DATE',
+        'uuid'           : 'UUID',
+        'still_available': 'BOOL',
+    }
+    pg_connector.cast_data_types(table_name_to_upload, column_types)
+    print(f"Columns in '{table_name_to_upload}' table have been cast to the following data types: '{column_types}'")
+
+    # Update the respective columns as primary key
+    primary_key = 'product_code'
+
+    pg_connector.add_primary_key(table_name_to_upload, primary_key)
+    print(f"'{primary_key}' in '{table_name_to_upload}' designated as primary key")
