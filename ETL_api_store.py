2cef185597cc8f5178d7cad729da3d511743c07f
Nick Armstrong <150172835+ChefData@users.noreply.github.com>
2023-12-19 20:54:05 +0000

    Update ETL_api_store.py
    Adds casting of data types and setting of primary keys

--- ETL_api_store.py
+++ ETL_api_store.py
@@ -41,3 +41,24 @@ if __name__ == "__main__":
     table_name_to_upload = 'dim_store_details'
     pg_connector.upload_to_db(cleaned_store_data, table_name_to_upload)
     print(f"Data uploaded to the '{table_name_to_upload}' table in the 'sales_data' PostgreSQL database.")
+
+    # Cast data types - The ? in VARCHAR will be replaced with an integer representing the maximum length of the values in that column.
+    column_types = {
+        'store_type'   : 'VARCHAR(255)',
+        'locality'     : 'VARCHAR(255)',
+        'continent'    : 'VARCHAR(255)',
+        'country_code' : 'VARCHAR(?)',
+        'store_code'   : 'VARCHAR(?)',
+        'staff_numbers': 'SMALLINT',
+        'opening_date' : 'DATE',
+        'longitude'    : 'FLOAT',
+        'latitude'     : 'FLOAT',
+    }
+    pg_connector.cast_data_types(table_name_to_upload, column_types)
+    print(f"Columns in '{table_name_to_upload}' table have been cast to the following data types: '{column_types}'")
+
+    # Update the respective columns as primary key
+    primary_key = 'store_code'
+
+    pg_connector.add_primary_key(table_name_to_upload, primary_key)
+    print(f"'{primary_key}' in '{table_name_to_upload}' designated as primary key")
